4c6e36bc195dc00267143b2c3b6487104891d49b
NOEL MELIA <G00237491@gmit.ie>
2019-01-23 15:48:22 +0000

    new file

---
+++ type.ts
@@ -0,0 +1,10 @@
+let flag: boolean = false;
+console.log("Value assigned to flag is "+flag);
+let decimal: number = 6;
+console.log("Value assigned to decimal is "+decimal);
+let color: string = "blue";
+console.log("Value assigned to string is "+color);
+let notSure: any = 4;
+notSure = "maybe a string instead";
+console.log("Value assigned to any is "+notSure);
+
