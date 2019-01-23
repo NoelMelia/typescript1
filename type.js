0ae07ecc3896bd937a9750f50c8c7b433b6a7f33
NOEL MELIA <G00237491@gmit.ie>
2019-01-23 15:50:09 +0000

    all

---
+++ type.js
@@ -0,0 +1,9 @@
+var flag = false;
+console.log("Value assigned to flag is " + flag);
+var decimal = 6;
+console.log("Value assigned to decimal is " + decimal);
+var color = "blue";
+console.log("Value assigned to string is " + color);
+var notSure = 4;
+notSure = "maybe a string instead";
+console.log("Value assigned to any is " + notSure);
